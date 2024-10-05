f4df1683239696c82144ab75793d40c1ab4e60fa
4udofruct1k <4udofructik@gmail.com>
2024-10-05 14:48:12 +0500

    Add files via upload

--- homework5/homework5.py
+++ homework5/homework5.py
@@ -1,15 +1,18 @@
 
 sum = 0
-cnt = 0
+fs = 0
+fe = 0
 while True:
+    if fs == 1 and fe == 1:
+            break
     num = input("Введите число: ")
     if num.isdigit() or num == "stop" or num == "end":
-        if num == "stop" or num == "end":
-            cnt += 1
+        if num == "stop":
+            fs = 1
+        elif num == "end":
+            fe = 1
         else:
-            sum += int(num)
+            sum += float(num)
     else:
         print("Введена буква")
-    if cnt == 2:
-        break
 print(sum)
